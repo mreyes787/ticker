8f495fb300e39cd0cb3775845ef53d80174ce49d
Maricela Patrickios <mreyes777@gmail.com>
2020-08-06 17:04:58 -0400

    Fun

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Ticker extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    }
+  }
+
+  componentDidMount(){
+    setInterval(() => {
+       this.setState({
+         count: this.state.count + 1
+       })
+    },1000)
+  }
+
+shouldComponentUpdate(nextProps, nextState){
+   if (nextState.count % 3 === 0) return true;
+   else return false;
 }
 
-export default App;
+  render() {
+    return(
+      <div>
+        <p>The ticker number is: {this.state.count}</p>
+      </div>
+    )
+  }
+}
+
+export default Ticker
